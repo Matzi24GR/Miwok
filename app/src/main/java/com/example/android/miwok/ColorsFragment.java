690bab2b692979b8400483ab8460b6ccce0583cc
Geomat Matzi <ggeomat@gmail.com>
2017-09-04 16:27:49 +0300

    Initial Commit

--- app/src/main/java/com/example/android/miwok/ColorsFragment.java
+++ app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -59,7 +59,7 @@ public class ColorsFragment extends Fragment {
     }
 
     public ColorsFragment() {
-        // Required empty public construct
+        // Required empty public constructor
 
     }
 
